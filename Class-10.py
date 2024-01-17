2f1a575295d0269b2c5d9dec2600eac6316c8aac
manojgain1988 <manojgain1988@gmail.com>
2024-01-17 18:33:31 +0600

    4th commit

--- Class-10.py
+++ Class-10.py
@@ -1,3 +1,25 @@
 
+class student:
+    def __init__(self,name,age,mobile,address):
+        self.name = name
+        self.age = age
+        self.phone = mobile
+        self.address = address
+    print()
+    def student_info(self):
+        print(f"Student Name is: {self.name}\nstudent age is: {self.age}\nstudent Phone number is: {self.phone}\nstudent address is: {self.address}")
+        
+    def python(self, programming):
+        print(f"{programming}")
+        
+        
+s1 = student("Manoj Gain", "33", "01911715356", "Kotalipara")
+s1.student_info()
 print()
-print("Hello Manoj Gain + Bithi Mondal")
+
+s2 = student("Bithi Gain", "23", "01911715356", "Kotalipara")
+s2.student_info()
+
+
+
+
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,2 @@
-
-except:
+8", "01911715356", "Kotalipara")
+print(s3.python())
