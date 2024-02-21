71014c18173824a8c645ccbc5c616f3f2abf141a
manojgain1988 <manojgain1988@gmail.com>
2024-02-21 12:05:56 +0600

    Create Django project

--- django-class-1.py
+++ django-class-1.py
@@ -2,10 +2,44 @@ python --version                               [check python version]
 pip --version                                  [check pip version]
 django-admin --version                         [check django version]
 pip install virtualenvwrapper-win              [For virtual envirenment setup create]
-mkvirtualenv projectenv(env_name)              [For virtual envirenment create]
+mkvirtualenv projectenv(env_name)/OR           [For virtual envirenment create]
+python -m venv myenv(env_name)                 [For virtual envirenment create]
+#myenv\Scripts\activate                        [For env active]
+pip list                                       [For check which package Install]
 workon (env_name)                              [For manually env open by other cmd]
-pip install django                             [for django install]
-mkdir django_project                           [for create folder]
+mkdir project folder(django_project)           [for create folder]
 cd django_project(folder_name)                 [For go folder_name]
+pip install django                             [for django install]
+pip list (again)                               [For check which django package]
+django-admin                                   [For check which django Tools/subcommands]
 django-admin startproject first(project_name)  [for Create project Name]
+python manage.py startapp mainapp(app_name)    [for create app]
 python manage.py runserver                     [for server run]
+
+
+
+
+
+
+
+
+***** project Folder create  secquence by secquence with venv by Vscode/CMD *****
+
+At first select Directory/Folder
+
+pip install virtualenvwrapper-win               [For virtual envirenment setup create]
+mkdir django-project (folder_name)              [Create folder name]
+cd django-project                               [For go folder]
+python -m venv myenv(env_name)/OR               [For create env] 
+mkvirtualenv projectenv(env_name)               [For virtual envirenment create]
+#myenv\Scripts\activate                         [For myenv(env_name) active] 
+pip list                                        [for check which install]    
+pip install django                              [For django install]
+pip list (again)                                [for check properly django install]    
+django-admin startproject first(project_name)   [For create project name]
+cd first(project_name)                          [For go project_name]
+python manage.py runserver                      [For project Run]
+python manage.py startapp mainapp(app_name)     [for create app]
+
+
+
