235a880736fe6415037b831b382fb3d6a5ed514e
manojgain1988 <manojgain1988@gmail.com>
2024-02-21 16:50:41 +0600

    django project manu

--- django-class-1.py
+++ django-class-1.py
@@ -24,18 +24,16 @@ python manage.py runserver                     [for server run]
 
 At first select Directory/Folder
 
-pip install virtualenvwrapper-win               [For virtual envirenment setup create]
 mkdir django-project (folder_name)              [Create folder name]
 cd django-project                               [For go folder]
+pip install virtualenvwrapper-win               [For virtual envirenment setup create]
 python -m venv myenv(env_name)/OR               [For create virtual envirenment] 
 #myenv\Scripts\activate                         [For myenv(env_name) active] 
-pip list                                        [for check which install]    
-pip install django                              [For django install]
-pip list (again)                                [for check properly django install]    
-django-admin startproject first(project_name)   [For create project name]
+pip install django                              [For django install]   
+django-admin startproject first .(project_name) [For create project name]
 cd first(project_name)                          [For go project_name]
-python manage.py runserver                      [For project Run]
 python manage.py startapp mainapp(app_name)     [for create app]
+python manage.py runserver                      [For project Run]
 
 
 
