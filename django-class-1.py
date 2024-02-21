0b7114cc45f354056733b3f036e4254c437e5424
manojgain1988 <manojgain1988@gmail.com>
2024-02-21 13:36:01 +0600

    some added

--- django-class-1.py
+++ django-class-1.py
@@ -2,8 +2,7 @@ python --version                               [check python version]
 pip --version                                  [check pip version]
 django-admin --version                         [check django version]
 pip install virtualenvwrapper-win              [For virtual envirenment setup create]
-mkvirtualenv projectenv(env_name)/OR           [For virtual envirenment create]
-python -m venv myenv(env_name)                 [For virtual envirenment create]
+python -m venv myenv(env_name) or              [For virtual envirenment create]
 #myenv\Scripts\activate                        [For env active]
 pip list                                       [For check which package Install]
 workon (env_name)                              [For manually env open by other cmd]
@@ -12,11 +11,9 @@ cd django_project(folder_name)                 [For go folder_name]
 pip install django                             [for django install]
 pip list (again)                               [For check which django package]
 django-admin                                   [For check which django Tools/subcommands]
-django-admin startproject first(project_name)  [for Create project Name]
+django-admin startproject first .(project_name)[for Create project Name same Folder]
 python manage.py startapp mainapp(app_name)    [for create app]
 python manage.py runserver                     [for server run]
-
-
 
 
 
@@ -30,8 +27,7 @@ At first select Directory/Folder
 pip install virtualenvwrapper-win               [For virtual envirenment setup create]
 mkdir django-project (folder_name)              [Create folder name]
 cd django-project                               [For go folder]
-python -m venv myenv(env_name)/OR               [For create env] 
-mkvirtualenv projectenv(env_name)               [For virtual envirenment create]
+python -m venv myenv(env_name)/OR               [For create virtual envirenment] 
 #myenv\Scripts\activate                         [For myenv(env_name) active] 
 pip list                                        [for check which install]    
 pip install django                              [For django install]
